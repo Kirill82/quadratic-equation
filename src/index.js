a1161ee99a7975eab6b5142a5a4f575626e1c8b0
Kirill Gorbikov <kirillhor@gmail.com>
2018-09-22 15:50:20 +0300

    sorting helps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var a = +equation.split(' * x^2')[0];
+  var b = equation.split(' * x^2 ')[1].split(' * x ')[0];
+  if (b.indexOf('-') != -1) {
+      b = -b.split(' ')[1];
+  } else {
+      b = +b.split(' ')[1];
+  }
+  var c = equation.split(' * x^2 ')[1].split(' * x ')[1];
+  if (c.indexOf('-') != -1) {
+      c = -c.split(' ')[1];
+  } else {
+      c = +c.split(' ')[1];
+  }
+
+  var x = [];
+  x[0] = Math.round((-1 * b - Math.sqrt(b * b - 4 * a * c))/ (2 * a));
+  x[1] = Math.round((-1 * b + Math.sqrt(b * b - 4 * a * c))/ (2 * a));
+  x.sort((a, b) => a - b);
+
+  return x;
+
 }
